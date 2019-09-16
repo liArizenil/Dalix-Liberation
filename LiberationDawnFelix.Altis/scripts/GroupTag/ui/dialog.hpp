d357bb98963f19673814c4ab9c5fba201f8333c8
Arizen <39409359+liArizenil@users.noreply.github.com>
2019-09-16 20:03:02 +0900

    sadas
    asdasd

--- LiberationDawnFelix.Altis/scripts/GroupTag/ui/dialog.hpp
+++ LiberationDawnFelix.Altis/scripts/GroupTag/ui/dialog.hpp
@@ -6,12 +6,21 @@ class liberation_GroupManager
 
 	objects[] = {};
 
-    class control
+    class controls
 	{
+        class Label_Header: RscText
+        {
+            idc = 1000;
+            text = "Group Name Setting"; //--- ToDo: Localize;
+            x = 0.29375 * safezoneW + safezoneX;
+            y = 0.368 * safezoneH + safezoneY;
+            w = 0.12375 * safezoneW;
+            h = 0.044 * safezoneH;
+        };
         class BG_Header: RscPicture
         {
             idc = 1200;
-            text = "#(argb,8,8,3)color(0,0.4,0,0.7)";
+            text = "#(argb,8,8,3)color(0,0,0,0.7)";
             x = 0.29375 * safezoneW + safezoneX;
             y = 0.379 * safezoneH + safezoneY;
             w = 0.4125 * safezoneW;
@@ -26,19 +35,10 @@ class liberation_GroupManager
             w = 0.4125 * safezoneW;
             h = 0.11 * safezoneH;
         };
-        class Label_Header: RscText
-        {
-            idc = 1000;
-            text = "Group Setting";
-            x = 0.29375 * safezoneW + safezoneX;
-            y = 0.379 * safezoneH + safezoneY;
-            w = 0.0721875 * safezoneW;
-            h = 0.022 * safezoneH;
-        };
         class Select_GroupTag: RscCombo
         {
             idc = 2100;
-            onLoad = "{ (_this select 0) lbAdd _x } foreach Group_Types; if(!isNil{ (group player) getVariable ['GroupType',nil] }) then { _index = Group_Types find ((group player) getVariable ['GroupType',nil]); (_this select 0) lbSetCurSel (_index) ; } else{ (_this select 0) lbSetCurSel 0 ; (group player) setVariable ['GroupType',GroupType select 0],true]; };";
+            onLoad = "{(_this select 0)lbAdd _x}foreach Group_Types;_index=(Group_Types find((group player)getVariable['GroupType',(Group_Types select 0)]));(_this select 0)lbSetCurSel(_index);(group player)setVariable['GroupType',Group_Types select _index,true];";
 			onLBSelChanged = "(group player) setVariable ['GroupType',lbText[ 2100 , (lbCurSel 2100)],true]";
 
             x = 0.298906 * safezoneW + safezoneX;
@@ -50,7 +50,6 @@ class liberation_GroupManager
         {
             idc = 1400;
             maxChars = 20;
-            onLoad = "ctrlSetText [1400, ((groupId (gorup player)) splitString ((group player) getVariable ['GroupType',''] + ' '))];";
 
             x = 0.4175 * safezoneW + safezoneX;
             y = 0.423 * safezoneH + safezoneY;
