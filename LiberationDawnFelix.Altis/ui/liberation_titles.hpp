35c0450d2172a09d73c205e9b1c1d5dafb596270
Arizen <39409359+liArizenil@users.noreply.github.com>
2019-10-01 21:46:34 +0900

    Update liberation_titles.hpp

--- LiberationDawnFelix.Altis/ui/liberation_titles.hpp
+++ LiberationDawnFelix.Altis/ui/liberation_titles.hpp
@@ -689,4 +689,135 @@ class RscTitles
 			};
 		};
 	};
+	class askbuild
+	{
+		name = "askbuild";
+		duration = 15;
+		idd = 5677;
+		movingEnable = false;
+		controlsBackground[] = {};
+
+		objects[] = {};
+
+		class controls
+		{
+			class Background: RscPicture
+			{
+				idc = 1200;
+				text = "#(argb,8,8,3)color(0,0,0,0.8)";
+				x = 0.840313 * safezoneW + safezoneX;
+				y = 0.016 * safezoneH + safezoneY;
+				w = 0.149531 * safezoneW;
+				h = 0.077 * safezoneH;
+			};
+			class Title_Background: RscPicture
+			{
+				idc = 1201;
+				text = "#(argb,8,8,3)color(0.3,0.3,0.3,0.9)";
+				x = 0.840312 * safezoneW + safezoneX;
+				y = 0.016 * safezoneH + safezoneY;
+				w = 0.118601 * safezoneW;
+				h = 0.0264 * safezoneH;
+			};
+			class Timer_Background: RscPicture
+			{
+				idc = 1202;
+				text = "#(argb,8,8,3)color(0.15,0.15,0.15,1)";
+				x = 0.958906 * safezoneW + safezoneX;
+				y = 0.016 * safezoneH + safezoneY;
+				w = 0.0309375 * safezoneW;
+				h = 0.0264 * safezoneH;
+			};
+			class Title: RscText
+			{
+				idc = 1000;
+				text = "%1 분대에서 %2 건설 요청"; //--- ToDo: Localize;
+
+				font = FontM;
+
+				x = 0.840312 * safezoneW + safezoneX;
+				y = 0.016 * safezoneH + safezoneY;
+				w = 0.117558 * safezoneW;
+				h = 0.0264 * safezoneH;
+			};
+			class Timer: RscText
+			{
+				idc = 1001;
+				text = "%1 초"; //--- ToDo: Localize;
+
+				font = FontM;
+
+				x = 0.958906 * safezoneW + safezoneX;
+				y = 0.016 * safezoneH + safezoneY;
+				w = 0.0309377 * safezoneW;
+				h = 0.0264 * safezoneH;
+			};
+			class label_OK: RscText
+			{
+				idc = 1002;
+				text = "동의"; //--- ToDo: Localize;
+
+				font = FontM;
+
+				x = 0.873312 * safezoneW + safezoneX;
+				y = 0.0446 * safezoneH + safezoneY;
+				w = 0.0319688 * safezoneW;
+				h = 0.044 * safezoneH;
+			};
+			class label_NO: RscText
+			{
+				idc = 1003;
+				text = "거부"; //--- ToDo: Localize;
+
+				font = FontM;
+
+				x = 0.939311 * safezoneW + safezoneX;
+				y = 0.0446 * safezoneH + safezoneY;
+				w = 0.04125 * safezoneW;
+				h = 0.044 * safezoneH;
+			};
+			class Key_OK: RscPicture
+			{
+				idc = 1203;
+				text = "#(argb,8,8,3)color(0.9,0.9,0.9,0.8)";
+				x = 0.845469 * safezoneW + safezoneX;
+				y = 0.0512 * safezoneH + safezoneY;
+				w = 0.0257812 * safezoneW;
+				h = 0.033 * safezoneH;
+			};
+			class Key_NO: RscPicture
+			{
+				idc = 1204;
+				text = "#(argb,8,8,3)color(0.9,0.9,0.9,0.8)";
+				x = 0.907354 * safezoneW + safezoneX;
+				y = 0.0512 * safezoneH + safezoneY;
+				w = 0.0268129 * safezoneW;
+				h = 0.033 * safezoneH;
+			};
+			class label_KEYOK: RscText
+			{
+				idc = 1004;
+				text = "HOME"; //--- ToDo: Localize;
+
+				font = FontM;
+
+				x = 0.844438 * safezoneW + safezoneX;
+				y = 0.0534 * safezoneH + safezoneY;
+				w = 0.022688 * safezoneW;
+				h = 0.0286 * safezoneH;
+			};
+			class label_KEYNO: RscText
+			{
+				idc = 1005;
+				text = "END"; //--- ToDo: Localize;
+
+				font = FontM;
+
+				x = 0.909416 * safezoneW + safezoneX;
+				y = 0.0534 * safezoneH + safezoneY;
+				w = 0.0247505 * safezoneW;
+				h = 0.0286 * safezoneH;
+			};
+		};
+	};
 };
