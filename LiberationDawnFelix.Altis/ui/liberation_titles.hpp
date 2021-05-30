a846f67c7cf1a235e91e4ebef4e1a20f475ce34a
Arizen <39409359+liArizenil@users.noreply.github.com>
2021-05-31 02:37:47 +0900

    xian sound remove

--- LiberationDawnFelix.Altis/ui/liberation_titles.hpp
+++ LiberationDawnFelix.Altis/ui/liberation_titles.hpp
@@ -7,12 +7,6 @@ class CfgSounds
 		sound[] = { "res\v44.ogg", 1.0, 1 };
 		titles[] = {};
 	};
-	class xianpara
-	{
-		name = "parasound";
-		sound[] = { "res\xian.ogg", 1.0, 1 };
-		titles[] = {};
-	};
 };
 
 class RscTitles
